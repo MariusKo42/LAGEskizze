97744282042fb8624b3d1168407faf0443367a9b
Felix Erdmann <felix.erdmann@uni-muenster.de>
2015-12-08 14:02:25 +0100

    First implementation of Einsätze

--- bob/routes.js
+++ bob/routes.js
@@ -1,24 +1,56 @@
 /*
 
-	Here comes the API.	
-	
+	Here comes the API.
+
 */
 
 
-app.get('/api/getBasemaps', function (req, res) {
-	// Hier müsste man die Basemaps holen	
+app.get('/api/getBasemaps', function(req, res) {
+  // Hier müsste man die Basemaps holen
 
   res.send('Hello World!');
 });
 
 
+app.get('/api/einsatz', function(req, res) {
+  models.einsaetze.find({}, function(error, values) {
+    if (error) {
+      var message = "DB error: " + error;
+      console.log(message);
+      res.status(400).send(message);
+    } else {
+      res.json(values);
+      res.end();
+    }
+  });
+});
 
-app.get('/api/getEinsatz/:EinsatzID/', function(req, res){
-	
-	// Beispiel wie das mit den Parametern läuft ;-)
-	
-	var einsatzid = req.params.EinsatzID;
+app.get('/api/einsatz/new', function(req, res) {
+	var myEinsatz = new models.einsaetze({});
 
-	
-	
+	myEinsatz.save(function(error) {
+    if (error) {
+      res.status(400).json({
+        status: "Fail creating paper DB entry for " + req.body.title + ": " + error
+      });
+    }
+  });
+});
+
+app.get('/api/einsatz/:EinsatzID/', function(req, res) {
+  var einsatzid = req.params.EinsatzID;
+
+	models.einsaetze.findById(einsatzid, function(err, value) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.json(value);
+      res.end();
+    }
+  });
+});
+
+app.get('/api/einsatz/:EinsatzID/lock', function(req, res) {
+  var einsatzid = req.params.EinsatzID;
+	//TODO: implememnt
 });
