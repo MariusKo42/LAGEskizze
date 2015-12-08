fd40b22a96d30222e2fa8ff32a1cb70b0c8258c4
nsteffens <n.steffens@me.com>
2015-12-08 13:40:06 +0100

    Routes.js created
    - Vorbereitung für public API-Integration
    - External files included

--- bob/index.js
+++ bob/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+require(../mongoose/db.js);
+require(../mongoose/models.js);
+require(./routes.js);
+
 app.get('/', function (req, res) {
   console.log('Got Request!');	
   res.send('Hello World!');
 });
+
 
 var server = app.listen(8080, function () {
   var host = server.address().address;
