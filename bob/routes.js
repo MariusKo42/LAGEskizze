ca4ed51cd90373c0faeb2c560251ccbc742e6e90
LEinfeldt <lasseeinfeldt@gmail.com>
2015-12-09 12:13:33 +0100

    Routen hinzugefuegt (delete/put)

--- bob/routes.js
+++ bob/routes.js
@@ -4,6 +4,13 @@
 	
 */
 
+/**
+* @desc Routes provided for the API
+*/
+
+var db = require(../mongoose/db.js);
+var models = require(../mongoose/models.js);
+var express = require('express');
 
 app.get('/api/getBasemaps', function (req, res) {
 	// Hier müsste man die Basemaps holen	
@@ -21,4 +28,39 @@ app.get('/api/getEinsatz/:EinsatzID/', function(req, res){
 
 	
 	
+});
+
+/**
+* @desc Speichere ein uebergebenes TZ in der DB
+*/
+app.put('/zeichen', function(req, res) {
+
+	var zeichen = new models.taktischeZeichen({
+		_id: ....;
+		datei: ...;
+		// Hier müsste gespeichert werden
+	});
+
+	zeichen.save(function(error) {
+		var message = error ? 'failed to save TZ:' + error
+							: 'saved TZ:' zeichen._id;
+		console.log(message);
+		res.send('{"id": "' + zeichen._id + '"}'); 
+	});
+});
+
+
+/**
+* @desc Loescht ein Taktisches Zeichen aus der Datenbank.
+* @param :id ID des TZ, das aus der DB geloescht werden soll
+*/
+app.delete('/zeichen/:id', function(req, res) {
+
+	var id = req.params.id;
+	models.taktischeZeichen.remove({_id: id}, function(error) {
+		var message = error ? 'failed to remove from DB' + error
+							: 'successfully deleted';
+		console.log(message);
+		res.send(message);
+	});
 });
