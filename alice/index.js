e0b348f9a8c4f225595a1d4c25103b7ed7f03b01
LEinfeldt <lasseeinfeldt@gmail.com>
2016-01-19 19:39:31 +0100

    Final Sync

--- alice/index.js
+++ alice/index.js
@@ -9,6 +9,7 @@ var models = require('./mongoose/models.js');
 var bodyParser = require('body-parser');
 var express = require('express');
 var request = require('request');
+var async = require('async');
 var app = express();
 
 app.use('/', express.static(__dirname));
@@ -30,44 +31,46 @@ app.post('/private/einsatz/', function(req, res) {
 	var json = JSON.parse(body);
 	var h =  JSON.parse(json.einsaetze);
 	json.einsaetze = h;
-	console.log(json.einsaetze);
-	for(var i = 0; i < json.einsaetze.length; i++) {
-		
-		var elem = json.einsaetze[i];
-		console.log(elem.meta);
-		db.models.einsaetze.find({id: elem.id}, function(err, result) {
-			console.log('gesucht');
-			console.log(result);
-			if(result != []) {
-				//erzeige neuen Einsatz in der DB, falls er noch nicht drin war.
-				var neuerEinsatz = new db.models.einsaetze({
-					id: elem.id,
-					meta: {
-						einsatzstichwort: elem.meta.einsatzstichwort,
-						einsatzort: elem.meta.einsatzort,
-						meldender: elem.meta.meldender,
-						objektNr: elem.meta.objektNr,
-						datumUhrzeitGruppe: elem.meta.datumUhrzeitGruppe
-					},
-					drawnObjects: elem.drawnObjects,
-					map: elem.map,
-					taktZeichen: elem.taktZeichen,
-					locked: elem.locked		
-				});
-				//speichere neunen Einsatz
-				neuerEinsatz.save();
-				//speichere Wert, der die Synchronitaet verneint
-				same = false;
-			}
-			else {console.log('gefunden')};
+	function checkDB(data, callback) {
+		async.each(data.einsaetze, function(file) {
+
+			db.models.einsaetze.findOne({id: file.id}, function(err, result) {
+				if(result == null) {
+					//erzeige neuen Einsatz in der DB, falls er noch nicht drin war.
+					var neuerEinsatz = new db.models.einsaetze({
+						id: file.id,
+						meta: {
+							einsatzstichwort: file.meta.einsatzstichwort,
+							einsatzort: file.meta.einsatzort,
+							meldender: file.meta.meldender,
+							objektNr: file.meta.objektNr,
+							datumUhrzeitGruppe: file.meta.datumUhrzeitGruppe
+						},
+						drawnObjects: file.drawnObjects,
+						map: file.map,
+						taktZeichen: file.taktZeichen,
+						locked: file.locked		
+					});
+					//speichere neunen Einsatz
+					neuerEinsatz.save();
+					console.log(neuerEinsatz);
+					//speichere Wert, der die Synchronitaet verneint
+					same = false;
+				}
+			});
 		});
+		setTimeout(function() {	
+			if(same) {
+				callback(1);
+			}
+			else callback(0);
+		}, 1500);
 	}
-	if(!same) {
-		res.send('Datenbank auf Alice wurde aktualisiert.');
-	}
-	else {
-		res.send('Datenbank auf Alice war synchron.');
-	}
+	checkDB(json, function(status) {
+		if(status == 0) res.send('Datenbank wurde synchronisiert.');
+		else if(status == 1) res.send('Datenbank war synchron.');
+	});
+
 });
 
 //start the server on Port 3000
