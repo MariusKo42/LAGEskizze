20f137624f20cad056b37640a6d9c46c8fc6c5dd
LEinfeldt <lasseeinfeldt@gmail.com>
2016-01-24 17:52:32 +0100

    allow headers

--- bob/index.js
+++ bob/index.js
@@ -13,6 +13,13 @@ var app = express();
 
 app.use('/', express.static(__dirname));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+  next();
+});
+
 //use the extended request body
 app.use(bodyParser.urlencoded({
   extended: true
