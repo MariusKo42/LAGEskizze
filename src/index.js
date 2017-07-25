b844b5a4b3282ba079fb06e776c4cb69db15b904
bzi <Bastian.Zimmer@eftas.com>
2017-07-25 14:23:22 +0200

    Variable declaration renewed Const browserwindow removed

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
 const windowManager = require('electron-window-manager');
 
 // Keep a global reference of the window object, if you don't, the window will
